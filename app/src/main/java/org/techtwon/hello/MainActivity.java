de13dba8b4ee9ee5587b635674586ca03650cd0a
DDing77 <smg76262585@gmail.com>
2021-09-25 05:17:50 +0900

    View_padding_margin

--- app/src/main/java/org/techtwon/hello/MainActivity.java
+++ app/src/main/java/org/techtwon/hello/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.weight);
     }
     public void onButton1Clicked(View v){
         Toast.makeText(this, "확인1 버튼이 눌렸어요", Toast.LENGTH_LONG).show();
