5918274b63a84ce83213c1f62a1357ab9a3b24e9
DDing77 <smg76262585@gmail.com>
2021-09-30 14:23:26 +0900

    Create SampleDrawable.java

---
+++ app/src/main/java/org/techtwon/hello/SampleDrawable.java
@@ -0,0 +1,15 @@
+package org.techtwon.hello;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+public class SampleDrawable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_drawable);
+    }
+}
