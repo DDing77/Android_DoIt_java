bb5ae1e48904f99a1e3d8976d6d83b4cb5f9fd46
DDing77 <smg76262585@gmail.com>
2021-10-09 22:50:37 +0900

    Create Mission6

---
+++ app/src/main/java/org/techtwon/hello/Mission06.java
@@ -0,0 +1,46 @@
+package org.techtwon.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class Mission06 extends AppCompatActivity {
+    SeekBar seekBar;
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission06);
+
+        textView = findViewById(R.id.textView4);
+        progressBar = findViewById(R.id.progressBar2);
+        seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(String.valueOf(progress));
+                progressBar.setProgress(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
+
+
+
+
+    }
+}
